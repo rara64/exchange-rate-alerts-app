77eb5e25d01018db8ce82209d057fa5a02a060cc
rara64 <60274052+rara64@users.noreply.github.com>
2026-04-20 11:28:19 +0200

    Working login code, needs cleanup

--- app/src/main/java/com/example/exchangeratealerts/LoginActivity.java
+++ app/src/main/java/com/example/exchangeratealerts/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.exchangeratealerts;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.exchangeratealerts.dtos.JWT;
+import com.example.exchangeratealerts.dtos.Login;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 public class LoginActivity extends AppCompatActivity {
+
+    APIInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +31,27 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        TextView textview = findViewById(R.id.textView);
+
+        apiInterface = APIClient.getRetrofitClient().create(APIInterface.class);
+
+        Login login = new Login();
+        login.username = "testuser";
+        login.password = "TestPass123";
+        Call<JWT> loginUser = apiInterface.login(login);
+        loginUser.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                Log.d("TAG",response.code()+"");
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable throwable) {
+
+            }
         });
     }
 }
